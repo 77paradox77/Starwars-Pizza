698672b9bee09b81c342665b6f7c39eb5e02c572
Reed Lambier <rlambier46@gmail.com>
2016-04-08 15:39:49 -0700

    Will add all working javascript except for pricing

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 // BUSINESS LOGIC
-function pizzaOrder(name, toppings, size) {
+function pizzaOrder(name, sauces, veggies, meat, size) {
   this.name = name;
   this.sauces = sauces;
   this.veggies = veggies;
   this.meat = meat;
   this.size = size;
+  this.price = "";
 }
 
-pizzaOrder.prototype.spacePrice = function() {
-  if (this.size === "small") {
-    return 250;
-  } else if (this.size === "medium") {
-    return 500;
-  } else if (this.size === "large") {
-    return 1000;
+var price = pizzaOrder.prototype.spacePrice = function() {
+  if (this.orderSize === "small") {
+    this.price = "250";
+  } else if (this.orderSize === "medium") {
+    this.price = "500";
+  } else if (this.orderSize === "large") {
+    price = "1000";
   } else {
     alert("please select a size!");
     }
@@ -30,13 +31,19 @@ $(document).ready(function() {
     var newOrder = new pizzaOrder (orderSauce, orderVeggies, orderMeat, orderSize);
     event.preventDefault();
 
-    $("ul#orders").append("<li><span class='pizza'>" + newOrder.pizzaOrder + "</span></li>");
+    $("ul#orders").append("<li><span class='pizza'>" + name + newOrder.pizzaOrder + "</span></li>");
 
     $(".pizza").last().click(function() {
       console.log("mekin moonay");
       $("#show-order").show();
-      $("show-order h2").text(newOrder.name);
-      $(".customer").last().text(newOrder.name);
+      $("#show-order h2").text(name);
+      $(".customer1").text(newOrder.name);
+      $(".customer2").text(newOrder.sauces);
+      $(".customer3").text(newOrder.veggies);
+      $(".customer4").text(newOrder.meat);
+      $(".customer5").text(newOrder.size);
+
+      $(".pricing").append(price);
   })
 });
 });
